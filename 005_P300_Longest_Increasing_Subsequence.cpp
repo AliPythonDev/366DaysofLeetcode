a0f2d199ad0e3247601d68c1ef2cbdda5e14d8c2
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-05 11:30:24 +0500

    Create 005_P300_Longest_Increasing_Subsequence.cpp

---
+++ 005_P300_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,24 @@
+/* 
+Day: 5
+Problem Number: 300 (https://leetcode.com/problems/longest-increasing-subsequence/)
+Date: 05-01-2024 
+Description:
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+Code:  */
+class Solution {
+ public:
+  int lengthOfLIS(vector<int>& nums) {
+    if (nums.empty())
+      return 0;
+    // dp[i] := the length of LIS ending in nums[i]
+    vector<int> dp(nums.size(), 1);
+
+    for (int i = 1; i < nums.size(); ++i)
+      for (int j = 0; j < i; ++j)
+        if (nums[j] < nums[i])
+          dp[i] = max(dp[i], dp[j] + 1);
+
+    return ranges::max(dp);
+  }
+};
