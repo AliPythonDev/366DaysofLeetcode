aba812601f8c730dcac1035d6eb7a39d9f7f547d
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-18 09:11:19 +0500

    Create 0018_P70_Climbing_Stairs.cpp

---
+++ 0018_P70_Climbing_Stairs.cpp
@@ -0,0 +1,38 @@
+/* 
+Day: 18
+Problem Number: 70 (https://leetcode.com/problems/climbing-stairs)
+Date: 18-01-2024 
+Description:
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+Constraints:
+* 1 <= n <= 45
+ 
+Code:  */
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; ++i) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
