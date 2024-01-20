fe9474517039a039ec85db7a739463bba087e906
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-20 16:27:49 +0500

    Create 0020_P907_Sum_of_Subarray_Minimums.cpp

---
+++ 0020_P907_Sum_of_Subarray_Minimums.cpp
@@ -0,0 +1,59 @@
+/* 
+Day: 20
+Problem Number: 907 (https://leetcode.com/problems/sum-of-subarray-minimums)
+Date: 20-01-2024 
+Description:
+Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 10^9 + 7.
+ 
+Example 1:
+Input: arr = [3,1,2,4]
+Output: 17
+Explanation: 
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+Sum is 17.
+
+Example 2:
+Input: arr = [11,81,94,43,3]
+Output: 444
+ 
+Constraints:
+* 1 <= arr.length <= 3 * 104
+* 1 <= arr[i] <= 3 * 104
+
+Code:  */
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> left(n, -1);
+        vector<int> right(n, n);
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && arr[stk.top()] >= arr[i]) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                left[i] = stk.top();
+            }
+            stk.push(i);
+        }
+        stk = stack<int>();
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stk.empty() && arr[stk.top()] > arr[i]) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                right[i] = stk.top();
+            }
+            stk.push(i);
+        }
+        long long ans = 0;
+        const int mod = 1e9 + 7;
+        for (int i = 0; i < n; ++i) {
+            ans += 1LL * (i - left[i]) * (right[i] - i) * arr[i] % mod;
+            ans %= mod;
+        }
+        return ans;
+    }
+};
