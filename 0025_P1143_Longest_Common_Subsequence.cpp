20bd3a251c1506259dddd604315003cf71df7ce8
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-25 11:38:09 +0500

    Create 0025_P1143_Longest_Common_Subsequence.cpp

---
+++ 0025_P1143_Longest_Common_Subsequence.cpp
@@ -0,0 +1,48 @@
+/* 
+Day: 25
+Problem Number: 1143 (https://leetcode.com/problems/longest-common-subsequence/)
+Date: 25-01-2024 
+Description:
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+* For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Example 1:
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+
+Example 2:
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+
+Example 3:
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+Constraints:
+* 1 <= text1.length, text2.length <= 1000
+* text1 and text2 consist of only lowercase English characters.
+
+Code:  */
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        int f[m + 1][n + 1];
+        memset(f, 0, sizeof f);
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                } else {
+                    f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        return f[m][n];
+    }
+};
