dc4a73571a002a9ad0f662682ff5466d59a9c2ef
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-02-07 11:50:47 +0500

    Create 0038_P451_Sort_Characters_By_Frequency.cpp

---
+++ 0038_P451_Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,52 @@
+/* 
+Day: 38
+Problem Number:  451 (https://leetcode.com/problems/sort-characters-by-frequency/)
+Date: 07-02-2024 
+Description:
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+Return the sorted string. If there are multiple answers, return any of them.
+ 
+Example 1:
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+Constraints:
+* 1 <= s.length <= 5 * 105
+* s consists of uppercase and lowercase English letters and digits.
+
+Code:  */
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> cnt;
+        for (char& c : s) {
+            ++cnt[c];
+        }
+        vector<char> cs;
+        for (auto& [c, _] : cnt) {
+            cs.push_back(c);
+        }
+        sort(cs.begin(), cs.end(), [&](char& a, char& b) {
+            return cnt[a] > cnt[b];
+        });
+        string ans;
+        for (char& c : cs) {
+            ans += string(cnt[c], c);
+        }
+        return ans;
+    }
+};
