d57d7d52b970e5b00fc6ebea02921b15a3491afe
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-03-07 09:39:19 +0500

    Create 0067_P876_Middle_of_the_Linked_List.cpp

---
+++ 0067_P876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,44 @@
+ /* 
+Day: 67
+Problem Number: 876 (https://leetcode.com/problems/middle-of-the-linked-list)
+Date: 07-03-2024
+Description:
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+Constraints:
+* The number of nodes in the list is in the range [1, 100].
+* 1 <= Node.val <= 100
+
+Code:  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
