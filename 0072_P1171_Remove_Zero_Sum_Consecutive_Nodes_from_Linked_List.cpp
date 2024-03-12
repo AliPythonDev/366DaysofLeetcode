1e48cc97a5f4ad084abcbb504f3455000a73c043
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-03-12 10:26:46 +0500

    Create 0072_P1171_Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List.cpp

---
+++ 0072_P1171_Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List.cpp
@@ -0,0 +1,59 @@
+/* 
+Day: 72
+Problem Number: 1171 (https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list)
+Date: 12-03-2024
+Description:
+Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+After doing so, return the head of the final linked list.  You may return any such answer.
+(Note that in the examples below, all sequences are serializations of ListNode objects.)
+
+Example 1:
+Input: head = [1,2,-3,3,1]
+Output: [3,1]
+Note: The answer [1,2,1] would also be accepted.
+
+Example 2:
+Input: head = [1,2,3,-3,4]
+Output: [1,2,4]
+
+Example 3:
+Input: head = [1,2,3,-3,-2]
+Output: [1]
+ 
+Constraints:
+* The given linked list will contain between 1 and 1000 nodes.
+* Each node in the linked list has -1000 <= node.val <= 1000.
+
+Code:  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head);
+        unordered_map<int, ListNode*> last;
+        ListNode* cur = dummy;
+        int s = 0;
+        while (cur) {
+            s += cur->val;
+            last[s] = cur;
+            cur = cur->next;
+        }
+        s = 0;
+        cur = dummy;
+        while (cur) {
+            s += cur->val;
+            cur->next = last[s]->next;
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
