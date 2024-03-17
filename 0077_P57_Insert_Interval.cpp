f496f66897f5afbfd430ae61ba480ad2ee674cb5
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-03-17 05:24:52 +0500

    Create 0077_P57_Insert_Interval.cpp

---
+++ 0077_P57_Insert_Interval.cpp
@@ -0,0 +1,55 @@
+/* 
+Day: 77
+Problem Number: 57 (https://leetcode.com/problems/insert-interval)
+Date: 17-03-2024
+Description:
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+Return intervals after the insertion.
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
+Constraints:
+* 0 <= intervals.length <= 10^4
+* intervals[i].length == 2
+* 0 <= starti <= endi <= 10^5
+* intervals is sorted by starti in ascending order.
+* newInterval.length == 2
+* 0 <= start <= end <= 10^5
+
+Code:  */
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int st = newInterval[0], ed = newInterval[1];
+        bool insert = false;
+        for (auto& interval : intervals) {
+            int s = interval[0], e = interval[1];
+            if (ed < s) {
+                if (!insert) {
+                    ans.push_back({st, ed});
+                    insert = true;
+                }
+                ans.push_back(interval);
+            } else if (e < st) {
+                ans.push_back(interval);
+            } else {
+                st = min(st, s);
+                ed = max(ed, e);
+            }
+        }
+        if (!insert) {
+            ans.push_back({st, ed});
+        }
+        return ans;
+    }
+};
