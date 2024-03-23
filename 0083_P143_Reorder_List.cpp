59902718ae860d9973bb732bb8d7fafcb6f66715
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-03-23 07:42:47 +0500

    Create 0083_P143_Reorder_List.cpp

---
+++ 0083_P143_Reorder_List.cpp
@@ -0,0 +1,64 @@
+/* 
+Day: 83
+Problem Number: 143 (https://leetcode.com/problems/reorder-list)
+Date: 23-03-2024
+Description:
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+Constraints:
+* The number of nodes in the list is in the range [1, 5 * 10^4].
+* 1 <= Node.val <= 1000
+ 
+Code:  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* cur = slow->next;
+        slow->next = nullptr;
+        ListNode* pre = nullptr;
+        while (cur) {
+            ListNode* t = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = t;
+        }
+        cur = head;
+
+        while (pre) {
+            ListNode* t = pre->next;
+            pre->next = cur->next;
+            cur->next = pre;
+            cur = pre->next;
+            pre = t;
+        }
+    }
+};
