0940dc255ce8dad6a190a38a55688205a262b07f
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-04-06 13:36:54 +0500

    Create 0097_P1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp

---
+++ 0097_P1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp
@@ -0,0 +1,60 @@
+/* 
+Day: 97
+Problem Number: 1249 (https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses)
+Date: 06-04-2024
+Description:
+Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+Formally, a parentheses string is valid if and only if:
+* It is the empty string, contains only lowercase characters, or
+* It can be written as AB (A concatenated with B), where A and B are valid strings, or
+* It can be written as (A), where A is a valid string.
+
+Example 1:
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s[i] is either'(' , ')', or lowercase English letter.
+
+Code:  */
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        string stk;
+        int x = 0;
+        for (char& c : s) {
+            if (c == ')' && x == 0) continue;
+            if (c == '(')
+                ++x;
+            else if (c == ')')
+                --x;
+            stk.push_back(c);
+        }
+        string ans;
+        x = 0;
+        while (stk.size()) {
+            char c = stk.back();
+            stk.pop_back();
+            if (c == '(' && x == 0) continue;
+            if (c == ')')
+                ++x;
+            else if (c == '(')
+                --x;
+            ans.push_back(c);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
