2161a575eafbe08b0d9937a8cd88a52b0874c66b
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-04-12 05:06:18 +0500

    Create 0103_P42_Trapping_Rain_Water.cpp

---
+++ 0103_P42_Trapping_Rain_Water.cpp
@@ -0,0 +1,39 @@
+/* 
+Day: 103
+Problem Number: 42 (https://leetcode.com/problems/trapping-rain-water)
+Date: 12-04-2024
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+Constraints:
+* n == height.length
+* 1 <= n <= 2 * 10^4
+* 0 <= height[i] <= 10^5
+
+Code:  */
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left[n], right[n];
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+        for (int i = 1; i < n; ++i) {
+            left[i] = max(left[i - 1], height[i]);
+            right[n - i - 1] = max(right[n - i], height[n - i - 1]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += min(left[i], right[i]) - height[i];
+        }
+        return ans;
+    }
+};
