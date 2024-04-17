7f8b58ebeea265283a76d15eb81fd9b3bf47ca7f
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-04-17 11:04:39 +0500

    Update 0105_P404_Sum_of_Left_Leaves.cpp

--- 0105_P404_Sum_of_Left_Leaves.cpp
+++ 0105_P404_Sum_of_Left_Leaves.cpp
@@ -2,6 +2,7 @@
 Day: 105
 Problem Number: 404 (https://leetcode.com/problems/sum-of-left-leaves)
 Date: 14-04-2024
+Description:
 Given the root of a binary tree, return the sum of all left leaves.
 A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 
