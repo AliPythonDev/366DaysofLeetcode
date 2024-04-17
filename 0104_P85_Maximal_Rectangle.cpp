717c853a8253d16ac8ab498d68797bac4178805e
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-04-17 11:05:24 +0500

    Update 0104_P85_Maximal_Rectangle.cpp

--- 0104_P85_Maximal_Rectangle.cpp
+++ 0104_P85_Maximal_Rectangle.cpp
@@ -2,6 +2,7 @@
 Day: 104
 Problem Number: 85 (https://leetcode.com/problems/maximal-rectangle)
 Date: 13-04-2024
+Description:
 Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
 
 Example 1:
