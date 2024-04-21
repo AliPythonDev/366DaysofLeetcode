53baea5bd8cce26ec85bcd4a197d63976d1c923d
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-04-21 10:48:53 +0500

    Create 0112_P1971_Find_if_Path_Exists_in_Graph.cpp

---
+++ 0112_P1971_Find_if_Path_Exists_in_Graph.cpp
@@ -0,0 +1,45 @@
+/* 
+Day: 112
+Problem Number: 1971 (https://leetcode.com/problems/find-if-path-exists-in-graph)
+Date: 21-04-2024
+Description:
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+Example 1:
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+
+Example 2:
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+
+Constraints:
+* 1 <= n <= 2 * 10^5
+* 0 <= edges.length <= 2 * 10^5
+* edges[i].length == 2
+* 0 <= ui, vi <= n - 1
+* ui != vi
+* 0 <= source, destination <= n - 1
+* There are no duplicate edges.
+* There are no self edges.
+
+Code:  */
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<int> p(n);
+        iota(p.begin(), p.end(), 0);
+        function<int(int)> find = [&](int x) -> int {
+            if (p[x] != x) p[x] = find(p[x]);
+            return p[x];
+        };
+        for (auto& e : edges) p[find(e[0])] = find(e[1]);
+        return find(source) == find(destination);
+    }
+};
