23799d1c4dcab8893fb157887010c448f1cb6000
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-05-08 05:30:31 +0500

    Create 0129_P506_Relative_Ranks.cpp

---
+++ 0129_P506_Relative_Ranks.cpp
@@ -0,0 +1,46 @@
+/* 
+Day: 129
+Problem Number: 506 (https://leetcode.com/problems/relative-ranks)
+Date: 08-05-2024
+Description:
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+* The 1st place athlete's rank is "Gold Medal".
+* The 2nd place athlete's rank is "Silver Medal".
+* The 3rd place athlete's rank is "Bronze Medal".
+* For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+ 
+Example 1:
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+
+Example 2:
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+Constraints:
+* n == score.length
+* 1 <= n <= 10^4
+* 0 <= score[i] <= 10^6
+* All the values in score are unique.
+
+Code:  */
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<pair<int, int>> idx;
+        for (int i = 0; i < n; ++i)
+            idx.push_back(make_pair(score[i], i));
+        sort(idx.begin(), idx.end(),
+            [&](const pair<int, int>& x, const pair<int, int>& y) { return x.first > y.first; });
+        vector<string> ans(n);
+        vector<string> top3 = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        for (int i = 0; i < n; ++i)
+            ans[idx[i].second] = i < 3 ? top3[i] : to_string(i + 1);
+        return ans;
+    }
+};
