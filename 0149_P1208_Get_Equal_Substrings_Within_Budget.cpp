17eb5412acf46a4076106a74ae1764d549fbf79e
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-05-28 11:09:42 +0500

    Create 0149_P1208_Get_Equal_Substrings_Within_Budget.cpp

---
+++ 0149_P1208_Get_Equal_Substrings_Within_Budget.cpp
@@ -0,0 +1,62 @@
+/* 
+Day: 149
+Problem Number: 1208 (https://leetcode.com/problems/get-equal-substrings-within-budget)
+Date: 28-05-2024
+Description:
+You are given two strings s and t of the same length and an integer maxCost.
+You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
+Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+ 
+Example 1:
+Input: s = "abcd", t = "bcdf", maxCost = 3
+Output: 3
+Explanation: "abc" of s can change to "bcd".
+That costs 3, so the maximum length is 3.
+
+Example 2:
+Input: s = "abcd", t = "cdef", maxCost = 3
+Output: 1
+Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+
+Example 3:
+Input: s = "abcd", t = "acde", maxCost = 0
+Output: 1
+Explanation: You cannot make any change, so the maximum length is 1.
+ 
+Constraints:
+* 1 <= s.length <= 10^5
+* t.length == s.length
+* 0 <= maxCost <= 10^6
+* s and t consist of only lowercase English letters.
+
+Code:  */
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        int f[n + 1];
+        f[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            f[i + 1] = f[i] + abs(s[i] - t[i]);
+        }
+        auto check = [&](int x) -> bool {
+            for (int i = 0; i + x - 1 < n; ++i) {
+                int j = i + x - 1;
+                if (f[j + 1] - f[i] <= maxCost) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        int l = 0, r = n;
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (check(mid)) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
