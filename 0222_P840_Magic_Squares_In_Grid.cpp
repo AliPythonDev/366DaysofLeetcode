c307e5a946fbd6375fb19118037e75245c103389
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-08-09 08:11:41 +0500

    Create 0222_P840_Magic_Squares_In_Grid.cpp

---
+++ 0222_P840_Magic_Squares_In_Grid.cpp
@@ -0,0 +1,79 @@
+/*
+Day: 222
+Problem Number: 840 (https://leetcode.com/problems/magic-squares-in-grid)
+Date: 09-08-2024
+Description:
+A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both diagonals all have the same sum.
+Given a row x col grid of integers, how many 3 x 3 contiguous magic square subgrids are there?
+Note: while a magic square can only contain numbers from 1 to 9, grid may contain numbers up to 15.
+
+Example 1:
+￼
+Input: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]]
+Output: 1
+Explanation: 
+The following subgrid is a 3 x 3 magic square:
+￼
+while this one is not:
+￼
+In total, there is only one magic square inside the given grid.
+
+Example 2:
+Input: grid = [[8]]
+Output: 0
+ 
+Constraints:
+* row == grid.length
+* col == grid[i].length
+* 1 <= row, col <= 10
+* 0 <= grid[i][j] <= 15
+
+Code:  */
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        auto check = [&](int i, int j) {
+            if (i + 3 > m || j + 3 > n) {
+                return 0;
+            }
+            vector<int> cnt(16);
+            vector<int> row(3);
+            vector<int> col(3);
+            int a = 0, b = 0;
+            for (int x = i; x < i + 3; ++x) {
+                for (int y = j; y < j + 3; ++y) {
+                    int v = grid[x][y];
+                    if (v < 1 || v > 9 || ++cnt[v] > 1) {
+                        return 0;
+                    }
+                    row[x - i] += v;
+                    col[y - j] += v;
+                    if (x - i == y - j) {
+                        a += v;
+                    }
+                    if (x - i + y - j == 2) {
+                        b += v;
+                    }
+                }
+            }
+            if (a != b) {
+                return 0;
+            }
+            for (int k = 0; k < 3; ++k) {
+                if (row[k] != a || col[k] != a) {
+                    return 0;
+                }
+            }
+            return 1;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans += check(i, j);
+            }
+        }
+        return ans;
+    }
+};
