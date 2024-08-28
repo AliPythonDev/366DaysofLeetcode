db4699cd1ca55692bf2db1b4a1309f44abc5c4d8
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-08-28 10:28:42 +0500

    Create 0241_P1905_Count_Sub_Islands.cpp

---
+++ 0241_P1905_Count_Sub_Islands.cpp
@@ -0,0 +1,55 @@
+/*
+Day: 241
+Problem Number: 1905 (https://leetcode.com/problems/count-sub-islands)
+Date: 28-08-2024
+Description:
+You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's (representing land). An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
+An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
+Return the number of islands in grid2 that are considered sub-islands.
+
+Example 1:
+Input: grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]
+Output: 3
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are three sub-islands.
+
+Example 2:
+Input: grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]
+Output: 2 
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
+ 
+Constraints:
+* m == grid1.length == grid2.length
+* n == grid1[i].length == grid2[i].length
+* 1 <= m, n <= 500
+* grid1[i][j] and grid2[i][j] are either 0 or 1.
+
+Code:  */
+class Solution {
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int m = grid1.size(), n = grid1[0].size();
+        int ans = 0;
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        function<int(int, int)> dfs = [&](int i, int j) {
+            int ok = grid1[i][j];
+            grid2[i][j] = 0;
+            for (int k = 0; k < 4; ++k) {
+                int x = i + dirs[k], y = j + dirs[k + 1];
+                if (x >= 0 && x < m && y >= 0 && y < n && grid2[x][y]) {
+                    ok &= dfs(x, y);
+                }
+            }
+            return ok;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid2[i][j]) {
+                    ans += dfs(i, j);
+                }
+            }
+        }
+        return ans;
+    }
+};
