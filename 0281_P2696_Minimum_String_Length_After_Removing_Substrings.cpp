7565550fa5e05ebaedc0b38e532e8fa7cb42d513
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-10-07 20:48:37 +0500

    Create 0281_P2696_Minimum_String_Length_After_Removing_Substrings.cpp

---
+++ 0281_P2696_Minimum_String_Length_After_Removing_Substrings.cpp
@@ -0,0 +1,57 @@
+/*
+Day: 281
+Problem Number: 2696 (https://leetcode.com/problems/minimum-string-length-after-removing-substrings)
+Date: 07-10-2024
+Description:
+You are given a string s consisting only of uppercase English letters.
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+Return the minimum possible length of the resulting string that you can obtain.
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+ 
+Example 1:
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+
+Example 2:
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+ 
+Constraints:
+* 1 <= s.length <= 100
+* s consists only of uppercase English letters.
+
+Code:  */
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s[i];
+
+            if (stack.empty()) {
+                stack.push(cur_char);
+                continue;
+            }
+
+            if (cur_char == 'B' && stack.top() == 'A') {
+                stack.pop();
+            }
+            else if (cur_char == 'D' && stack.top() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur_char);
+            }
+        }
+
+        return stack.size();
+    }
+};
