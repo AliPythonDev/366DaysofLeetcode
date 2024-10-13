6367acfe32b04a32125f6ed5f4b23daf32568c58
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-10-13 09:40:18 +0500

    Update 0287_P632_Smallest_Range_Covering_Elements_from_K_Lists.cpp

--- 0287_P632_Smallest_Range_Covering_Elements_from_K_Lists.cpp
+++ 0287_P632_Smallest_Range_Covering_Elements_from_K_Lists.cpp
@@ -22,7 +22,7 @@ Constraints:
 * nums.length == k
 * 1 <= k <= 3500
 * 1 <= nums[i].length <= 50
-* -105 <= nums[i][j] <= 10^5
+* -10^5 <= nums[i][j] <= 10^5
 * nums[i] is sorted in non-decreasing order.
 
 Code:  */
