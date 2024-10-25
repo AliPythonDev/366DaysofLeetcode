a265bad4c47b5a32035653099c25ca928c129a12
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-10-25 08:48:07 +0500

    Create 0299_P1233_Remove_Sub-Folders_from_the_Filesystem.cpp

---
+++ 0299_P1233_Remove_Sub-Folders_from_the_Filesystem.cpp
@@ -0,0 +1,47 @@
+/*
+Day: 299
+Problem Number: 1233 (https://leetcode.com/problems/remove-sub-folders-from-the-filesystem)
+Date: 25-10-2024
+Description:
+Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
+If a folder[i] is located within another folder[j], it is called a sub-folder of it. A sub-folder of folder[j] must start with folder[j], followed by a "/". For example, "/a/b" is a sub-folder of "/a", but "/b" is not a sub-folder of "/a/b/c".
+The format of a path is one or more concatenated strings of the form: '/' followed by one or more lowercase English letters.
+* For example, "/leetcode" and "/leetcode/problems" are valid paths while an empty string and "/" are not.
+ 
+Example 1:
+Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+Output: ["/a","/c/d","/c/f"]
+Explanation: Folders "/a/b" is a subfolder of "/a" and "/c/d/e" is inside of folder "/c/d" in our filesystem.
+
+Example 2:
+Input: folder = ["/a","/a/b/c","/a/b/d"]
+Output: ["/a"]
+Explanation: Folders "/a/b/c" and "/a/b/d" will be removed because they are subfolders of "/a".
+
+Example 3:
+Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+ 
+Constraints:
+* 1 <= folder.length <= 4 * 10^4
+* 2 <= folder[i].length <= 100
+* folder[i] contains only lowercase letters and '/'.
+* folder[i] always starts with the character '/'.
+* Each folder name is unique.
+
+Code:  */
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans = {folder[0]};
+        for (int i = 1; i < folder.size(); ++i) {
+            int m = ans.back().size();
+            int n = folder[i].size();
+            if (m >= n || !(ans.back() == folder[i].substr(0, m) && folder[i][m] == '/')) {
+                ans.emplace_back(folder[i]);
+            }
+        }
+        return ans;
+    }
+};
