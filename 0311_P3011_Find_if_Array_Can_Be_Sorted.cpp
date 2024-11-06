5c39a5ade742b916af3d4538b403cc5feebdae42
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-11-06 06:54:45 +0500

    Update 0311_P3011_Find_if_Array_Can_Be_Sorted.cpp

--- 0311_P3011_Find_if_Array_Can_Be_Sorted.cpp
+++ 0311_P3011_Find_if_Array_Can_Be_Sorted.cpp
@@ -6,6 +6,7 @@ Description:
 You are given a 0-indexed array of positive integers nums. 
 In one operation, you can swap any two adjacent elements if they have the same number of set bits. You are allowed to do this operation any number of times (including zero).
 Return true if you can sort the array, else return false.
+
 Example 1:
 Input: nums = [8,4,2,30,15]
 Output: true
