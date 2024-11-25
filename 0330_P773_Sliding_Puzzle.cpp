b601f1baecb865de9d8c61e11ca58f95411f3ac9
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-11-25 10:46:25 +0500

    Create 0330_P773_Sliding_Puzzle.cpp

---
+++ 0330_P773_Sliding_Puzzle.cpp
@@ -0,0 +1,110 @@
+/*
+Day: 330
+Problem Number: 773 (https://leetcode.com/problems/sliding-puzzle)
+Date: 25-11-2024
+Description:
+On an 2 x 3 board, there are five tiles labeled from 1 to 5, and an empty square represented by 0. A move consists of choosing 0 and a 4-directionally adjacent number and swapping it.
+The state of the board is solved if and only if the board is [[1,2,3],[4,5,0]].
+Given the puzzle board board, return the least number of moves required so that the state of the board is solved. If it is impossible for the state of the board to be solved, return -1.
+
+Example 1:
+Input: board = [[1,2,3],[4,0,5]]
+Output: 1
+Explanation: Swap the 0 and the 5 in one move.
+
+Example 2:
+Input: board = [[1,2,3],[5,4,0]]
+Output: -1
+Explanation: No number of moves will make the board solved.
+
+Example 3:
+Input: board = [[4,1,2],[5,0,3]]
+Output: 5
+Explanation: 5 is the smallest number of moves that solves the board.
+An example path:
+After move 0: [[4,1,2],[5,0,3]]
+After move 1: [[4,1,2],[0,5,3]]
+After move 2: [[0,1,2],[4,5,3]]
+After move 3: [[1,0,2],[4,5,3]]
+After move 4: [[1,2,0],[4,5,3]]
+After move 5: [[1,2,3],[4,5,0]]
+
+Constraints:
+* board.length == 2
+* board[i].length == 3
+* 0 <= board[i][j] <= 5
+* Each value board[i][j] is unique.
+
+Code:  */
+class Solution {
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {
+        string start = gets(board);
+        string end = "123450";
+        if (start == end) return 0;
+        unordered_set<string> vis;
+        vis.insert(start);
+        queue<string> q{{start}};
+        int ans = 0;
+        while (!q.empty()) {
+            ++ans;
+            for (int n = q.size(); n > 0; --n) {
+                string x = q.front();
+                q.pop();
+                setb(x, board);
+                for (string y : next(board)) {
+                    if (y == end) return ans;
+                    if (!vis.count(y)) {
+                        vis.insert(y);
+                        q.push(y);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    string gets(vector<vector<int>>& board) {
+        string s = "";
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                s.push_back('0' + board[i][j]);
+        return s;
+    }
+
+    void setb(string s, vector<vector<int>>& board) {
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                board[i][j] = s[i * 3 + j] - '0';
+    }
+
+    vector<string> next(vector<vector<int>>& board) {
+        vector<string> res;
+        auto p = find0(board);
+        int i = p.first, j = p.second;
+        vector<int> dirs = {-1, 0, 1, 0, -1};
+        for (int k = 0; k < 4; ++k) {
+            int x = i + dirs[k], y = j + dirs[k + 1];
+            if (x >= 0 && x < 2 && y >= 0 && y < 3) {
+                swap(i, j, x, y, board);
+                res.push_back(gets(board));
+                swap(i, j, x, y, board);
+            }
+        }
+        return res;
+    }
+
+    void swap(int i, int j, int x, int y, vector<vector<int>>& board) {
+        int t = board[i][j];
+        board[i][j] = board[x][y];
+        board[x][y] = t;
+    }
+
+    pair<int, int> find0(vector<vector<int>>& board) {
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                if (board[i][j] == 0)
+                    return {i, j};
+        return {0, 0};
+    }
+};
